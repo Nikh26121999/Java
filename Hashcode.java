325a0e7c161477e66c7c52d2e6117a2d8f61dd89
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-25 17:15:44 +0000

    Hashcode.java

---
+++ Hashcode.java
@@ -0,0 +1,10 @@
+class Hashcode
+{
+    public static void main(String[] args)
+    {
+        String s1=new String("RajaRamaMohanRoy");
+        System.out.println(s1);
+        Hashcode s2=new Hashcode();
+        System.out.println(s2);       
+    }
+}
