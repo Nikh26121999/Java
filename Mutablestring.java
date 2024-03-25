cf152862b5cb15fbc9311d8586d8028790f65eb4
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-25 16:43:19 +0000

    Mutablestring.java

---
+++ Mutablestring.java
@@ -0,0 +1,10 @@
+class Mutablestring
+{
+    public static void main(String args[])
+    {
+        StringBuffer sb=new StringBuffer("Rama");
+        System.out.println(sb);
+        sb.append("Sita");
+        System.out.println(sb);
+    }
+}
