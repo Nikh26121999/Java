19dcf2b40d411f07be570d64a4c4a3d01f4f8188
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-26 13:37:58 +0000

    MethodNOIPANDOP.java

---
+++ MethodNOIPANDOP.java
@@ -0,0 +1,16 @@
+class MethodNOIPANDOP
+{
+    public static void main(String[] args) {
+        
+    }
+    void add()
+    {
+        int a;
+        int b;
+        int c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println(c);
+    }
+}
