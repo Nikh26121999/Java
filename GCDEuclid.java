be2a090ddc009b4e33465db1074d3747eb20b848
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-26 14:32:58 +0000

    GCDEuclid.java

---
+++ GCDEuclid.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class euclideanalgm
+{
+    int findGCD(int m,int n)
+    {
+        int rem;
+        while(n!=0)
+        {
+            rem=m%n;
+            m=n;
+            n=rem;
+        }
+        int gcd=m;
+        return gcd;
+
+    }
+}
+class GCDEuclid
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter a number and find gcd");
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        GCDEuclid ge=new GCDEuclid;
+        int res=ge.findGCD(a,b);
+        System.out.println("the gcd of" +a+ "and" +b+ "is" +res);
+    }    
+
+    private int findGCD(int a, int b) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
