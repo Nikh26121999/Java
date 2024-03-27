be3940cc88149334fc950b789881b9b966d282e5
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-27 16:33:57 +0000

    insertionsort.java

---
+++ insertionsort.java
@@ -0,0 +1,21 @@
+
+public class Insertionsort {
+	void insertionsortdemo(int a[])
+	{
+		int item;
+		int i,j;
+		
+		for(i=1;i<=a.length-1;i++)
+		{
+			item=a[i];
+			j=i-1;
+			while(j>=0 && a[j]>item)
+			{
+				a[j+1]=a[j];
+				--j;
+			}
+			a[j+1]=item;
+		}
+	}
+
+}
