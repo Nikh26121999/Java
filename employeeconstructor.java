4cbba1076e92f8cb7b5eca093eda084535ddcef1
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-29 13:56:43 +0000

    employeeconstructor.java

---
+++ employeeconstructor.java
@@ -0,0 +1,12 @@
+
+public class employeeconstructor {
+	public static void main(String args[])
+	{
+		constructor e1=new constructor("nikh",40000,123);
+		System.out.println(e1.getName());
+		System.out.println(e1.getSalary());
+		System.out.println(e1.getId());
+		
+	}
+
+}
