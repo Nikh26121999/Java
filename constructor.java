3425f8a22da747722a6b9195ad369ff09f4d0dec
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-29 13:54:54 +0000

    constructor.java

---
+++ constructor.java
@@ -0,0 +1,25 @@
+
+public class constructor {
+	private String name;
+	private int salary;
+	private int id;
+	public constructor(String a,int b,int c)
+	{
+		name=a;
+		salary=b;
+		id=c;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getSalary()
+	{
+		return salary;
+	}
+	public int getId()
+	{
+		return id;
+	}
+
+}
