587b41f10728cbf697d83395ff471acb904abc1d
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:33:23 +0000

    constructorusingthis.java

---
+++ constructorusingthis.java
@@ -0,0 +1,25 @@
+
+public class constructorusingthis {
+	private String name;
+	private int salary;
+	private int id;
+	public constructorusingthis(String name,int salary,int id)
+	{
+		this.name=name;
+		this.salary=salary;
+		this.id=id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getSalary()
+	{
+		return salary;
+	}
+	public int getId()
+	{
+		return id;
+	}
+
+}
