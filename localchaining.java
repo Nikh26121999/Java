14908136fe8cb4d22b58b8d4d4b13673805f1d5f
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:40:21 +0000

    localchaining.java

---
+++ localchaining.java
@@ -0,0 +1,30 @@
+
+public class localchaining {
+	int noofblades;
+	String brand;
+	String color;
+	public localchaining()
+	{
+		super();
+	}
+	public localchaining(int noofblades,String brand)
+	{
+		super();
+		this.noofblades=noofblades;
+		this.brand=brand;
+	}
+	public localchaining(String brand,String color)
+	{
+		super();
+		this.brand=brand;
+		this.color=color;
+	}
+	public localchaining(int noofblades,String brand,String color)
+	{
+		super();
+		this.noofblades=noofblades;
+		this.brand=brand;
+		this.color=color;
+	}
+
+}
