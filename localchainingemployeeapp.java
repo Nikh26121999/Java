7b1d906eb9d3c99ea44507c128c7df72e6558372
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:46:19 +0000

    localchainingemployeeapp.java

---
+++ localchainingemployeeapp.java
@@ -0,0 +1,12 @@
+
+public class localchainingemployeeapp {
+	public static void main(String args[])
+	{
+	localchainingemployee e1=new localchainingemployee("kod01",22,100000,"raj");
+	System.out.println(e1.ssn);
+	System.out.println(e1.age);
+	System.out.println(e1.salary);
+	System.out.println(e1.name);
+	}
+}
+	
