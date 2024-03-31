ec9f6efb4978e41ddff74f1949e2d949dd20aa98
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:35:59 +0000

    defaultconstructor.java

---
+++ defaultconstructor.java
@@ -0,0 +1,20 @@
+
+public class defaultconstructor {
+	private String name;
+	private int rollno;
+	private int marks;
+	public String getName()
+	{
+		return name;
+	}
+	public int getRollno()
+	{
+		return rollno;
+	}
+	public int getMarks()
+	{
+		return marks;
+	}
+	
+
+}
