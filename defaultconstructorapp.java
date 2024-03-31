fa094875b66814c4f8969e79a45ae5df938ef053
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:37:04 +0000

    defaultconstructorapp.java

---
+++ defaultconstructorapp.java
@@ -0,0 +1,11 @@
+
+public class defaultconstructorapp {
+	public static void main(String args[])
+	{
+		defaultconstructor d1=new defaultconstructor();
+		System.out.println(d1.getName());
+		System.out.println(d1.getRollno());
+		System.out.println(d1.getMarks());
+	}
+
+}
