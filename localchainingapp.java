69667cd5fd2664ffcc6901074f5c2fa52c4c8f7b
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:41:43 +0000

    localchainingapp.java

---
+++ localchainingapp.java
@@ -0,0 +1,10 @@
+
+public class localchainingapp {
+public static void main(String args[])
+{
+	localchaining l1=new localchaining(3,"usha","blue");
+	System.out.println(l1.noofblades);
+	System.out.println(l1.brand);
+	System.out.println(l1.color);
+}
+}
