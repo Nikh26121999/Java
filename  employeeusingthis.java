9057e1174cd70745fe1e7ada3787ead138a8aa41
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:34:33 +0000

    employeeusingthis.java

---
+++  employeeusingthis.java
@@ -0,0 +1,11 @@
+
+public class employeeusingthis {
+	public static void main(String args[])
+	{
+		constructorusingthis c1=new constructorusingthis("nikh",40000,123);
+		System.out.println(c1.getName());
+		System.out.println(c1.getSalary());
+		System.out.println(c1.getId());
+	}
+
+}
