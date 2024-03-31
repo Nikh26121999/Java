1d799297235bc5bdc041c3dce5d6d227c65bf630
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-03-31 16:44:27 +0000

    Fanapp.java

---
+++ Fanapp.java
@@ -0,0 +1,11 @@
+
+public class Fanapp {
+	public static void main(String args[])
+	{
+		Fan f1=new Fan("usha","blue");
+		System.out.println(f1.noofblades);
+		System.out.println(f1.brand);
+		System.out.println(f1.color);
+	}
+
+}
