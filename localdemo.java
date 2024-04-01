5da65bb31b83d6023f9f9382426ecedfd37d6f00
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-01 16:01:50 +0000

    localdemo.java

---
+++ localdemo.java
@@ -0,0 +1,19 @@
+
+public class localdemo {
+	int a=10;
+	static int b=20;
+	public void display()
+	{
+		int c=30;
+		System.out.println(c);
+		System.out.println(a);
+		System.out.println(b);
+	}
+	public static void display2()
+	{
+		int d=40;
+		System.out.println(d);
+		
+	}
+
+}
