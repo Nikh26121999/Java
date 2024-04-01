9362221f70a31d307ff9fedf3799d86f7804e2d4
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-01 15:57:42 +0000

    Businessmanapp.java

---
+++ Businessmanapp.java
@@ -0,0 +1,19 @@
+
+public class Businessmanapp {
+	public static void main(String args[])
+	{
+		Businessman b1=new Businessman();
+		Businessman b2=new Businessman();
+		Businessman b3=new Businessman();
+		b1.takeInput();
+		b1.CalculateSi();
+		b1.display();
+		b2.takeInput();
+		b2.CalculateSi();
+		b2.display();
+		b3.takeInput();
+		b3.CalculateSi();
+		b3.display();
+	}
+
+}
