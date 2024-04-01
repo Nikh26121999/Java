9405798cac6d7c182153d37e539362949d3f1ae9
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-01 16:04:17 +0000

    refobject.java

---
+++ refobject.java
@@ -0,0 +1,19 @@
+class std{
+}
+class emp{
+	
+}
+
+class refobject
+{
+	public std send1()
+	{
+		std a=new std();
+		return a;
+	}
+	public static emp send2()
+	{
+		emp b=new emp();
+		return b;
+	}
+}
