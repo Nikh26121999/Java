c96ea497c74e17d0024a5a5fc280aa39a698746e
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-01 15:59:45 +0000

    mdemo.java

---
+++  mdemo.java
@@ -0,0 +1,35 @@
+
+public class mdemo {
+int add(int x,int y)
+{
+	return x+y;
+}
+float add(int x,float y)
+{
+	return x+y;
+}
+double add(int x,float y,float z)
+{
+	return x+y+z;
+}
+float add(float x,float y,float z)
+{
+	return x+y+z;
+}
+double add(float x,float y,double z)
+{
+	return x+y+z;
+}
+float add(float x,int y)
+{
+	return x+y;
+}
+float add(int x,int y,float z)
+{
+	return x+y+z;
+}
+float add(float x,float y)
+{
+	return x+y;
+}
+}
