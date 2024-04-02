8c9ed7d56ff1ce9d92f5c225dc198d587d407db2
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-02 13:05:30 +0000

    tiger.java

---
+++ tiger.java
@@ -0,0 +1,15 @@
+
+public class tiger extends Animal {
+	String gender;
+	public tiger()
+	{
+		this("male");
+		
+	}
+	public tiger(String gender)
+	{
+		super("prath",26);
+		this.gender=gender;
+	}
+
+}
