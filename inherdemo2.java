ab74b732648e60206e564a9a81173935910ad9a2
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-02 13:02:08 +0000

    inherdemo2.java

---
+++ inherdemo2.java
@@ -0,0 +1,21 @@
+
+public class inherdemo2 extends constructinherit {
+	int c;
+	int d;
+	public inherdemo2()
+	{
+		super();
+		a=10;
+		b=20;
+		c=30;
+		d=40;
+	}
+	public inherdemo2(int c,int d)
+	{
+		a=100;
+		b=200;
+		this.c=c;
+		this.d=d;
+	}
+
+}
