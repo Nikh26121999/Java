61d79f024ee42e7d5de58bf58d29b8fe8d46c5de
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-02 13:03:24 +0000

    inheritconstructapp.java

---
+++ inheritconstructapp.java
@@ -0,0 +1,12 @@
+
+public class inheritconstructapp {
+	public static void main(String args[])
+	{
+		inherdemo2 d2=new inherdemo2();
+		System.out.println(d2.a);
+		System.out.println(d2.b);
+		System.out.println(d2.c);
+		System.out.println(d2.d);
+	}
+
+}
