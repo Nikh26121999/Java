70ec98424e7125c781b9f516185496ee11f07466
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-02 13:06:47 +0000

    animalapp.java

---
+++ animalapp.java
@@ -0,0 +1,11 @@
+
+public class animalapp {
+	public static void main(String args[])
+	{
+		tiger t=new tiger();
+		System.out.println(t.name);
+		System.out.println(t.age);
+		System.out.println(t.gender);
+	}
+
+}
