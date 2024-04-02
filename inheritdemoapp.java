2a7cf4b286343da047fdb34b5f06511dc651f790
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-02 12:56:27 +0000

    inheritdemoapp.java

---
+++ inheritdemoapp.java
@@ -0,0 +1,15 @@
+
+public class inheritdemoapp {
+	public static void main(String args[])
+	{
+		inheritdemo1 d1=new inheritdemo1();
+		System.out.println(d1.c);
+		System.out.println(d1.a);
+		System.out.println(d1.b);
+		System.out.println(d1.d);
+		System.out.println("____________");
+		d1.display1();
+		d1.display2();
+	}
+
+}
