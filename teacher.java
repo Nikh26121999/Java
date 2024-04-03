697a113d7ecc8ed3cb2131ff154208a80c8f381f
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-03 16:38:21 +0000

    teacher.java

---
+++ teacher.java
@@ -0,0 +1,13 @@
+
+public class teacher {
+	public void takeattendnce()
+	{
+		System.out.println("teacher is taking attendance");
+	}
+	public void teach()
+	{
+		System.out.println("teacher is teaching");
+		
+	}
+
+}
