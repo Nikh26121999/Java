6c9cd87d836d5d2314ea827d4274c7fc4c92a9eb
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-03 16:39:50 +0000

    physicsteacher.java

---
+++ physicsteacher.java
@@ -0,0 +1,14 @@
+
+public class physicsteacher extends teacher
+{
+	public void teach()
+	{
+		System.out.println("physics teacher is teaching physics");
+	}
+	public void doexp()
+	{
+		System.out.println("physics teacher is doing law experiment");
+	}
+	
+
+}
