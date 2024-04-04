83c7968fa8704414131785e2f871926cb0a1a7c1
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-04 16:57:46 +0000

    charger.java

---
+++ charger.java
@@ -0,0 +1,19 @@
+
+public class charger {
+	String color;
+	String brand;
+	public charger(String brand ,String color)
+	{
+		this.color=color;
+		this.brand=brand;
+	}
+	public String getcolor()
+	{
+		return color;
+	}
+	public String getbrand()
+	{
+		return brand;
+	}
+
+}
