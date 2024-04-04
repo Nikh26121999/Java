520fddc90e33245e3a8afd9865f5ad2a6aa8da41
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-04 16:59:07 +0000

    os.java

---
+++ os.java
@@ -0,0 +1,19 @@
+
+public class os {
+	int type;
+	String name;
+	public os(int type,String name)
+	{
+		this.type=type;
+		this.name=name;
+	}
+	public int gettype()
+	{
+		return type;
+	}
+	public String getname()
+	{
+		return name;
+	}
+
+}
