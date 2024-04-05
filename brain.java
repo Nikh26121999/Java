c2bf175fbd6cee8d46b3c1aedb59ec2103ff79ee
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-05 17:06:57 +0000

    brain.java

---
+++ brain.java
@@ -0,0 +1,19 @@
+
+public class brain {
+	String color;
+	String weight;
+	public brain(String color,String weight)
+	{
+		this.color=color;
+		this.weight=weight;
+	}
+	public String getcolor()
+	{
+		return color;
+	}
+	public String getweight()
+	{
+		return weight;
+	}
+
+}
