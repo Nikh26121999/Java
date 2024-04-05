7db558eed2a9acce585dd94d18fcfe0eca2efd88
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-05 17:21:15 +0000

    abschemistryteacher.java

---
+++ abschemistryteacher.java
@@ -0,0 +1,11 @@
+
+public class abschemistryteacher extends absteacher{
+	public void teach()
+	{
+		System.out.println("chemistry teacher teaches chemistry");
+	}
+	public void takeattendance()
+	{
+		System.out.println("chemistry teacher takes attendance after lunch");
+	}
+}
