68083d38739ba8dd836c931f23053656c97f7707
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-05 17:19:18 +0000

    absphysicsteacher.java

---
+++ absphysicsteacher.java
@@ -0,0 +1,12 @@
+
+public class absphysicsteacher extends absteacher{
+	public void teach()
+	{
+		System.out.println("physics teacher is teaching physics");
+	}
+	public void takeattendance()
+	{
+		System.out.println("physics teacher takes attendance before lunch");
+	}
+
+}
