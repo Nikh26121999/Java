a1c9174e6c942ba3e7e9ff8920e8b153799e708f
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-05 17:07:47 +0000

    heart.java

---
+++ heart.java
@@ -0,0 +1,19 @@
+
+public class heart {
+	String color;
+	String weight;
+	public heart(String color,String weight)
+	{
+		this.color=color;
+		this.weight=weight;
+	}
+	public String getcolor()
+	{
+		return color;
+	}
+	public String getweight()
+	{
+		return weight;
+	}
+
+}
