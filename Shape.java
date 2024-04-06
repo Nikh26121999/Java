ecedc1ac9fd13e358027893873ba1cea526fe588
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-06 16:31:29 +0000

    Shape.java

---
+++ Shape.java
@@ -0,0 +1,12 @@
+abstract class shape
+{
+    float area;
+    abstract public void takeInput();
+    public void ComputeA()
+    {
+         public void disA()
+        {
+            System.out.println("area="+area);
+        }
+    }
+}
