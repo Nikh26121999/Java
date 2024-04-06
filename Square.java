a3ff0c91709ffb7f0a6a2f8b6cd67bf0ae47ac8a
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-06 16:43:09 +0000

    Square.java

---
+++ Square.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Square extends Shape{
+    float len;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("please enter length of square");
+    len r=scan.nextInt();
+}
+public void computeA()
+{
+    area=len*len;
+}
