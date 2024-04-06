09c2f7b17d6a3dd0f4c86c6b70503975f30b9489
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-06 16:47:47 +0000

    Geometry.java

---
+++ Geometry.java
@@ -0,0 +1,14 @@
+public class Geometry
+{
+    public static void main(String args[])
+    {
+        Circle c=new Circle();
+        c.takeInput();
+        c.ComputeA();
+        c.dispA();
+        Square s=new Square();
+        s.takeInput();
+        s.computeA();
+        s.dispA();
+    }
+}
