470be333242436089390db69203a4c14d960e5e0
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-07 16:22:23 +0000

    Bird.java

---
+++ Bird.java
@@ -0,0 +1,5 @@
+abstract class Bird
+{
+    abstract public void fly();
+    abstract public void eat();
+}
---
+++ Eagle.java
@@ -0,0 +1,7 @@
+abstract class Eagle extends Bird{
+    public void fly()
+    {
+        System.out.println("eagle fly at high altitudes");
+        
+    }
+}
