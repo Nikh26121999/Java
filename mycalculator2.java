08518ff3d6a545807b5db7ab70f6141d03cd1a58
Nikh26121999 <149522192+Nikh26121999@users.noreply.github.com>
2024-04-08 16:55:27 +0000

    mycalculator2.java

---
+++ mycalculator2.java
@@ -0,0 +1,12 @@
+class mycalculator2 implements Calculator
+{
+    void mul()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter 2 numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a*b;
+        System.out.println(c);
+    }
+}
